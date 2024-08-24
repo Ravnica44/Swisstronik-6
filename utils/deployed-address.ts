edf601ed17585dddd2a50a7cd89e87e898db65ad
ravnica44 <ravnicaeu2@gmail.com>
2024-08-24 19:12:36 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xB87985491de30b4aa8c1C7074FA4F058589BD214'
+export const ProxyAdmin = '0xd98C80F2622af38534AAA6D54a1007722bF78A64'
+export const SWTRImplementation = '0x0A78750aF44Aa0B1Dd5aCfa29955941057296de6'
     
